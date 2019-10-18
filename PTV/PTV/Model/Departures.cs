60e2b6bc3c17e9dfc8391307a468640d2d1592dd
Jeel Patel <jeel.patel1111@gmail.com>
2019-10-19 02:27:20 +1100

    added few request functions to the departures.cs

--- PTV/PTV/Model/Departures.cs
+++ PTV/PTV/Model/Departures.cs
@@ -1,12 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Helpers;
+using Newtonsoft.Json.Linq;
 
-namespace PTV.Model
+namespace PTV
 {
-    class Departures
+    public class Departures
     {
+        // Constructures 
+        // Departure(Stop Type, Stop ID)
+        // Departure(Stop Type, Stop ID, Direction ID)
+        public static async Task<JObject> GetDepartures(int stop_id)
+        {
+            // the PTV api method we want
+            string url = "/v3/departures/route_type/0/stop/" + stop_id;
+            // calculating signature
+            string request = Signature.CalcuteSignature(url);
+            // sending request and returning the object
+            return await RequestPTV.SendRequest(request);
+        }
+
+        public static async Task<JObject> GetDepartures(int route_type, int stop_id)
+        {
+            // the PTV api method we want
+            string url = "/v3/departures/route_type/" + route_type + "/stop/" + stop_id;
+            Console.WriteLine(url);
+            // calculating signature
+            string request = Signature.CalcuteSignature(url);
+            // sending request and returning the object
+            return await RequestPTV.SendRequest(request);
+        }
+
+        public static async Task<JObject> GetDepartures(int route_type, int stop_id, int route_id)
+        {
+            // the PTV api method we want
+            string url = "/v3/departures/route_type/" + route_type + "/stop/" + stop_id + "/route/" + route_id;
+            Console.WriteLine(url);
+            // calculating signature
+            string request = Signature.CalcuteSignature(url);
+            // sending request and returning the object
+            return await RequestPTV.SendRequest(request);
+        }
     }
+
+
 }
