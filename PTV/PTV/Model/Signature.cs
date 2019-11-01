626defb2d4f654107018c2c732ef4faa82805c26
Thinh <thinhngo.1798@gmail.com>
2019-11-01 11:02:36 +1100

    Adding comment in signature

--- PTV/PTV/Model/Signature.cs
+++ PTV/PTV/Model/Signature.cs
@@ -41,7 +41,11 @@ namespace PTV
 
             return url;
         }
-
+        /// <summary>
+        /// To remove the white space in the input box to corresponding symbol of the signature.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public static string remove_white_spaces(string url)
         {
             string new_url = "";
